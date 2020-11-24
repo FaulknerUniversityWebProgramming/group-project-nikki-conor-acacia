01dcb68fd44f45d2bdd7e4a4162d7bcd9eb52868
Conor Gilpin <716614@faulkner.edu>
2020-11-24 05:50:01 +0000

    Added Img rotation for Mainpage in JS - Conor

--- js/MainPage/mainpage.js
+++ js/MainPage/mainpage.js
@@ -1,6 +1,9 @@
 var figure = document.querySelector("#featured");
 var image = figure.getElementsByTagName('img')[0];
-
+var images =["pictures/MainPage/medium/Clocktower.png",
+             "pictures/MainPage/medium/Court.png",
+             "pictures/MainPage/medium/Drive.png",
+             "pictures/MainPage/medium/HPBuilding.png"];
 document.getElementById("thumbnails").onclick = function(event) {
 let target = event.target;
 enlarge(target)
@@ -9,10 +12,18 @@ enlarge(target)
 
 function enlarge(target) {
         image.src = target.src.replace("small", "medium");
-        image.title = target.title;
-        
-        
+        image.title = target.title;    
 }
+var imageIndex = 0;
+function changeImage() {
+  image.setAttribute("src",images[imageIndex]);
+  imageIndex++;
+  if (imageIndex >= images.length) {
+    imageIndex = 0;
+  }
+}
+
+setInterval(changeImage,8000);
 
 
 
