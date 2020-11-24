c9498702c168cfed3694b40cd43f9c8f5356e2c1
Conor Gilpin <716614@faulkner.edu>
2020-11-24 01:49:48 +0000

    Added and Updated CSS,Header, Footer, and Main Page elements -Conor

--- js/MainPage/mainpage.js
+++ js/MainPage/mainpage.js
@@ -1,8 +1,5 @@
 var figure = document.querySelector("#featured");
 var image = figure.getElementsByTagName('img')[0];
-var caption = figure.getElementsByTagName('figcaption')[0];
-
-
 
 document.getElementById("thumbnails").onclick = function(event) {
 let target = event.target;
@@ -13,19 +10,9 @@ enlarge(target)
 function enlarge(target) {
         image.src = target.src.replace("small", "medium");
         image.title = target.title;
-        caption.innerHTML = target.title;
+        
         
 }
 
-image.onmouseover = function() {mymouseover()};
-image.onmouseout = function() {mymouseout()};
 
-window.onload = function mymouseover() {
-        caption.style.transition = "opacity 1s";
-        caption.style.opacity = 0.8;
-}
 
-window.onload = function mymouseout() {
-        caption.style.transition = "opacity 1s";
-        caption.style.opacity = 0;
-}
