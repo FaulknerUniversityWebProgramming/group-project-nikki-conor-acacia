09d9df3062f0ce2d2270ec2f19e74827fdc1d9e5
Xfairley717141 <46569218+Xfairley717141@users.noreply.github.com>
2020-11-24 11:52:15 -0600

    Update login.sql

--- sql/login.sql
+++ sql/login.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `user`(
-  `id`int(11) NOT NULL PRIMARY KEY AUTO,
-  `username` varchar(80) NOT NULL,
-  `name` varchar(80) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `email` varchar(80) NOT NULL
+CREATE TABLE user(
+  id int(11) NOT NULL PRIMARY KEY AUTO,
+  username varchar(80) NOT NULL,
+  name varchar(80) NOT NULL,
+  password varchar(80) NOT NULL,
+  email varchar(80) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`id`, `username`, `name`, `password`, `email` ) VALUES
-  (1, 'xfairley717141', 'Xfairley717141','Xyquirreah fairley', 'password123','Xyquirreah.fairley@faulkner.edu');
+INSERT INTO user (id, username, name, password, email) VALUES
+  (1, xfairley717141, Xfairley717141, Xyquirreah fairley, password123, Xyquirreah.fairley@faulkner.edu);
