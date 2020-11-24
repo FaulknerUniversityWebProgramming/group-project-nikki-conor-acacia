211ff4f942f2b6d319a9deeabf4b91f9df7fc5a7
Xfairley717141 <46569218+Xfairley717141@users.noreply.github.com>
2020-11-24 11:51:30 -0600

    Update home.php

--- home.php
+++ home.php
@@ -3,13 +3,13 @@ include "/databases/header.php";
 
 // Check user login or not
 if(!isset($_SESSION['email'])){
-    header('Location: /php/login.php');
+    header('Location: login.php');
 }
 
 // logout
 if(isset($_POST['but_logout'])){
     session_destroy();
-    header('Location: /php/login.php');
+    header('Location: MainFile.html');
 }
 ?>
 <?php
